4dc9143cd68e1de6ad658219b5f553bb564520bd
jdoi <jdoi.work@gmail.com>
2019-10-15 00:17:07 +0900

    heroku docker対応

---
+++ bin/build.sh
@@ -0,0 +1,1 @@
+stack build --stack-root $PWD/.stack-root
--- src/WebApp.hs
+++ src/WebApp.hs
@@ -2,11 +2,19 @@ module WebApp (app) where
 
 import Web.Scotty
 
+import System.Environment (lookupEnv)
+
 routes = do
   get "/" $ do
     text $ "hello scotty" 
 
+getPort :: IO Int
+getPort = do
+  envPort <- lookupEnv "PORT"
+  return $ read $ maybe "5000" id envPort
+
 app :: IO ()
 app = do
-  scotty 5000 routes
+  port <- getPort
+  scotty port routes
   
